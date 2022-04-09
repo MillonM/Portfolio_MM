f503bb5b446cead44eef9a8bc3d019e527deb95b
MillonM <martinmillonschik@gmail.com>
2022-04-09 20:29:22 -0300

    commit_6

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { InfoPersonalComponent } from './componentes/info-personal/info-personal.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    InfoPersonalComponent,
+    ExperienciaComponent,
+    SkillsComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
